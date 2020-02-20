895f9eddb661514c499ad4f47d33d05c99318585
Robert Ellis <robert.ellis@rightmove.co.uk>
2020-02-20 13:26:44 +0000

    Moved pin mode setings to InputTask and OutputTask

--- octasource/src/tasks/InputTask.cpp
+++ octasource/src/tasks/InputTask.cpp
@@ -8,6 +8,14 @@ InputTask::InputTask(CvInputOutput& cvInputOutput) :
 
 void InputTask::init() {
     Task::init();
+    _cvInputOutput.setPinModeAnalogIn(0);
+    _cvInputOutput.setPinModeAnalogIn(1);
+    _cvInputOutput.setPinModeAnalogIn(2);
+    _cvInputOutput.setPinModeAnalogIn(3);
+    _cvInputOutput.setPinModeAnalogIn(4);
+    _cvInputOutput.setPinModeAnalogIn(5);
+    _cvInputOutput.setPinModeAnalogIn(6);
+    _cvInputOutput.setPinModeAnalogIn(7);
 }
 
 void InputTask::execute() {
