455e5984674d7c42bb6b134ca6c978caf165d924
Robert Ellis <rob@pigatron-industries.co.uk>
2022-01-31 19:49:57 +0000

    Use ObjectSelector for list ot attractors

--- src/controllers/AttractorController.h
+++ src/controllers/AttractorController.h
@@ -15,6 +15,7 @@
 #include "systems/attractor/RabinovichFabrikantAttractor.h"
 #include "systems/attractor/ThreeScrollUnifiedAttractor.h"
 #include "systems/attractor/FourWingAttractor.h"
+#include "eurorack.h"
 
 #define ATTRACTOR_COUNT 12
 
@@ -31,60 +32,32 @@ class AttractorController : public Controller {
         LinearInput<OctasourceInputDevice> ampCvInput = LinearInput<OctasourceInputDevice>(Hardware::hw.ampCvPin, -5, 5, 0, 1);
         //LinearInput<OctasourceInputDevice> cCvInput = LinearInput<OctasourceInputDevice>(Hardware::hw.waveCvPin, -5, 5, 0, 5);
 
-        LorenzAttractor lorenzAttractor1;
-        LorenzAttractor lorenzAttractor2;
-        Lorenz83Attractor lorenz83Attractor1;
-        Lorenz83Attractor lorenz83Attractor2;
-        ThomasAttractor thomasAttractor1;
-        ThomasAttractor thomasAttractor2;
-        AizawaAttractor aizawaAttractor1;
-        AizawaAttractor aizawaAttractor2;
-        DadrasAttractor dadrasAttractor1;
-        DadrasAttractor dadrasAttractor2;
-        ChenAttractor chenAttractor1;
-        ChenAttractor chenAttractor2;
-        RosslerAttractor rosslerAttractor1;
-        RosslerAttractor rosslerAttractor2;
-        HalvorsenAttractor halvorsenAttractor1;
-        HalvorsenAttractor halvorsenAttractor2;
-        ChuaMultiScrollAttractor chuaMultiScrollAttractor1;
-        ChuaMultiScrollAttractor chuaMultiScrollAttractor2;
-        RabinovichFabrikantAttractor rabinovichFabrikantAttractor1;
-        RabinovichFabrikantAttractor rabinovichFabrikantAttractor2;
-        ThreeScrollUnifiedAttractor threeScrollUnifiedAttractor1;
-        ThreeScrollUnifiedAttractor threeScrollUnifiedAttractor2;
-        FourWingAttractor fourWingAttractor1;
-        FourWingAttractor fourWingAttractor2;
-
-        ContinuousSystemN<3>* attractors1[ATTRACTOR_COUNT] = {
-            &lorenzAttractor1,
-            &lorenz83Attractor1,
-            &thomasAttractor1,
-            &aizawaAttractor1,
-            &dadrasAttractor1,
-            &chenAttractor1,
-            &rosslerAttractor1,
-            &halvorsenAttractor1,
-            &chuaMultiScrollAttractor1,
-            &rabinovichFabrikantAttractor1,
-            &threeScrollUnifiedAttractor1,
-            &fourWingAttractor1
-        };
-
-        ContinuousSystemN<3>* attractors2[ATTRACTOR_COUNT] = {
-            &lorenzAttractor2,
-            &lorenz83Attractor2,
-            &thomasAttractor2,
-            &aizawaAttractor2,
-            &dadrasAttractor2,
-            &chenAttractor2,
-            &rosslerAttractor2,
-            &halvorsenAttractor2,
-            &chuaMultiScrollAttractor2,
-            &rabinovichFabrikantAttractor2,
-            &threeScrollUnifiedAttractor2,
-            &fourWingAttractor2
-        };
+        ObjectSelector<ContinuousSystemN<3>, 
+                        LorenzAttractor, 
+                        Lorenz83Attractor, 
+                        ThomasAttractor, 
+                        AizawaAttractor, 
+                        DadrasAttractor, 
+                        ChenAttractor, 
+                        RosslerAttractor, 
+                        HalvorsenAttractor, 
+                        ChuaMultiScrollAttractor, 
+                        RabinovichFabrikantAttractor, 
+                        ThreeScrollUnifiedAttractor, 
+                        FourWingAttractor> attractors1;
+        ObjectSelector<ContinuousSystemN<3>, 
+                        LorenzAttractor, 
+                        Lorenz83Attractor, 
+                        ThomasAttractor, 
+                        AizawaAttractor, 
+                        DadrasAttractor, 
+                        ChenAttractor, 
+                        RosslerAttractor, 
+                        HalvorsenAttractor, 
+                        ChuaMultiScrollAttractor, 
+                        RabinovichFabrikantAttractor, 
+                        ThreeScrollUnifiedAttractor, 
+                        FourWingAttractor> attractors2;
 
         float amp;
         
